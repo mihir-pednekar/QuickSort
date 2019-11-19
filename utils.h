45b7ec15efdc4271501d4754a3dbfbeb5ae75543
papajones03 <56189811+papajones03@users.noreply.github.com>
2019-11-19 05:52:46 +0000

    Update utils.h

--- utils.h
+++ utils.h
@@ -9,46 +9,9 @@ and fillArray with random no
 #include <iostream>
 #include <cstdlib>
 
-namespace util{
-	
-	//returns a random number between range min and max
-	int getRandom(int min, int max){
-    		int random = rand()%max + min;
-    		while(true){
-        		//std::cout<<"Test Random: "<<random<<"\n";
-        		if(random>=min && random<=max)
-        		    break;
-        		else
-        		    random = rand()%max + min;
-    		}
-	    return random;
-	}
-
-	//fills array of parameter size with random numbers using getRandom() function
-	void fillArray(int data[], int size, int min, int max){
-		for(int i=0;i<size;i++){
-			data[i]=getRandom(min,max);
-    		}
-	}
-
-	//prints array of parameter size
-	void printArray(int data[], int size){
-    		std::cout<<"[";
-    		for(int i=0;i<size;i++){
-        		std::cout<<data[i];
-        		if(i==size-1)
-        	    	std::cout<<"]\n";
-        		else
-        	    	std::cout<<",";
-    		}
-	}
-
-	// swap by reference.
-	void swap(int &a, int &b){
-		//if max is less than min then swap
-        	int temp=a;
-        	a=b;
-        	b=temp;
-	}
-
-}
+int partition(int data[], int left, int right);
+void sort(int data[], int left, int right);
+int getRandom(int min, int max);
+void fillArray(int data[], int size, int min, int max);
+void printArray(int data[], int size);
+void swap(int &a, int &b);
