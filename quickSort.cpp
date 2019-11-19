758c8febf1f9dc6725c1346e1d31d6bec7aa79cb
papajones03 <56189811+papajones03@users.noreply.github.com>
2019-11-19 01:56:26 +0000

    Added pseudo code for Quicksort

--- quickSort.cpp
+++ quickSort.cpp
@@ -1,3 +1,19 @@
+// author: Mihir Pednekar
+// Student Id: 19027656
+
+//Pseudo Code for Quick Sort
+/* 
+Step 1: Initialise Pivot element as the last element of an array as pivot=arr[size-1], pivot helps to partition elements of array.
+Step 2: Initialise left index 'l' as index of small element as l=-1 and right index 'r' as loop variable which is incremented every pass.
+Step 3: Every iteration check arr[r]<=pivot, if true then increment l++ and swap(arr[l], arr[r]) else do nothing.
+Step 4: Continue Step 3 until right index 'r' is less than index of pivot element as r<(size-1)
+Step 5: Every elements to the left of left index 'l' are less than equal to pivot element so swap pivot element with arr[l+1] and return partition
+	index value return l+1.
+Step 6: Pivot element separates into two partition having new left and right index, so steps 2 to 5 are repeated with new index until left index 
+	is not less than right, left<right
+ 
+*/
+
 #include <iostream>
 #include <cstdlib>
 
@@ -58,8 +74,6 @@ namespace QuickSort{
     
     void sort(int data[], int left, int right){
         if (left < right){ 
-            /* pi is partitioning index, arr[p] is now 
-            at right place */
             int partIndex = partition(data, left, right); 
   
             // Separately sort elements before 
